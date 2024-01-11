1ea0d71afefb76cdbf86d4445591dcb42bf87b22
amahadnezhad <amahadnezhad@gmail.com>
2024-01-12 01:12:17 +0330

    Add 'Add' & 'save' Method To cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,9 @@
 
 class Cart:
     def __init__(self, request):
+        """
+        Initialize The Cart
+        """
         self.request = request
         self.session = request.session
 
@@ -11,3 +14,22 @@ class Cart:
             # cart = self.session['cart']
 
         self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+        Add The Specified Product To The Cart If It Exists
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+
+        else:
+            self.cart[product_id][quantity] += quantity
+
+        self.save()
+
+    def save(self):
+        """
+        Mark Session As Modified To Save Changes
+        """
+        self.session.modified = True
