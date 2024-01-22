0e0839283d4d408a4b48a7ad527e5f326f33250e
amahadnezhad <amahadnezhad@gmail.com>
2024-01-23 00:58:30 +0330

    Add 'len' Method & 'clear' Method To Cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from products.models import Product
+
 
 class Cart:
     def __init__(self, request):
@@ -42,3 +44,28 @@ class Cart:
         Mark Session As Modified To Save Changes
         """
         self.session.modified = True
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product-obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    def __len__(self):
+        return len(self.cart.keys())
+
+    def clear(self):
+        del self.session['cart']
+        self.save()
+
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return sum(product.price for product in products)
