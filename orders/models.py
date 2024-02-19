f904d72ceecbdacdaf4f5156d055e206ec567739
amahadnezhad <amahadnezhad@gmail.com>
2024-02-19 17:27:22 +0330

    Add Order Model To Orders

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=700)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
