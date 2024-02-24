c64fedd8fa9ff793fc07f55ae8d2b3e7989a0171
amahadnezhad <amahadnezhad@gmail.com>
2024-02-24 23:25:34 +0330

    Make Some Change In Templates

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .cart import Cart
 from products.models import Product
@@ -32,7 +34,7 @@ def add_to_cart_view(request, product_id):
         quantity = cleaned_data['quantity']
         cart.add(product, quantity, replace_current_quantity=cleaned_data['inplace'])
 
-    return redirect('cart:cart_detail')
+    return redirect('product_list')
 
 
 def remove_from_cart(request, product_id):
@@ -42,3 +44,14 @@ def remove_from_cart(request, product_id):
 
     return redirect('cart:cart_detail')
 
+
+@require_POST
+def clear_cart(request):
+    cart = Cart(request)
+    if len(cart):
+        cart.clear()
+        messages.success(request, _("All Products Successfully Removed From Your Cart"))
+    else:
+        messages.warning(request, _("Your Cart Is Already Empty"))
+
+    return redirect("cart:cart_detail")
