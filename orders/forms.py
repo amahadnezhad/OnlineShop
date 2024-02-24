5afb88d953beb16cd6a3f2cec6eca7a30ed5473d
amahadnezhad <amahadnezhad@gmail.com>
2024-02-24 15:18:26 +0330

    Add Placeholder To OrderForm

--- orders/forms.py
+++ orders/forms.py
@@ -7,6 +7,16 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'phone_number', 'address', 'order_notes']
+        widgets = {
+            'order_notes': forms.Textarea(attrs={
+                'rows': 5,
+                'placeholder': 'If You Have Any Words Please Enter Here!',
+            }),
+            'address': forms.Textarea(attrs={
+                'rows': 3,
+                'placeholder': 'Enter Your Address Here!',
 
+            }),
+        }
 
 
